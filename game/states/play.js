769c1bd71ceedb92ee87d39227d0af141eaf0d42
Juho Leinonen <juholei@gmail.com>
2015-02-26 14:42:53 +0200

    Add missing comma

--- game/states/play.js
+++ game/states/play.js
@@ -17,12 +17,13 @@ Play.prototype = {
   createBoardFromGameData: function() {
     this.pointGroup = this.game.add.group();
     var data = this.game.data;
+
     var startPoint = this.game.add.sprite(data.startPoint.x, data.startPoint.y, 'start-end', 0);
     startPoint.anchor.setTo(0.5, 0.5);
     startPoint.scale.setTo(0.5, 0.5);
     this.pointGroup.add(startPoint);
 
-    var endPoint = this.game.add.sprite(data.endPoint.x, data.endPoint.y 'start-end', 1);
+    var endPoint = this.game.add.sprite(data.endPoint.x, data.endPoint.y, 'start-end', 1);
     endPoint.anchor.setTo(0.5, 0.5);
     endPoint.scale.setTo(0.5, 0.5);
     this.pointGroup.add(endPoint);
