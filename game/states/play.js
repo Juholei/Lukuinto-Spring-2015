8febdb027a0486b1d3eefa5d9251c320c258aa86
Juho Leinonen <juholei@gmail.com>
2015-02-26 14:40:41 +0200

    Add class GameData. Move declaration of the point locations to that class. Now in Play state this class is read and the Points are created there dynamically based on it.

--- game/states/play.js
+++ game/states/play.js
@@ -6,36 +6,34 @@ function Play() {}
 Play.prototype = {
   create: function() {
     this.backgroundMap = this.game.add.sprite(0, 0, 'map');
-    this.pointGroup = this.game.add.group();
-
-    var startPoint = this.game.add.sprite(246, 118, 'start-end', 0);
-    startPoint.anchor.setTo(0.5, 0.5);
-    startPoint.scale.setTo(0.5, 0.5);
-
-    var endPoint = this.game.add.sprite(797, 680, 'start-end', 1);
-    endPoint.anchor.setTo(0.5, 0.5);
-    endPoint.scale.setTo(0.5, 0.5);
-
-    this.avatar = new Avatar(this.game, 246, 143);
-    this.game.add.existing(this.avatar);
-
-    var point1 = new Point(this.game, 446, 101);
-    var point2 = new Point(this.game, 451, 407);
-    var point3 = new Point(this.game, 553, 483);
-    var point4 = new Point(this.game, 515, 616);
-
-    this.pointGroup.add(startPoint);
-    this.pointGroup.add(endPoint);
-    this.pointGroup.add(point1);
-    this.pointGroup.add(point2);
-    this.pointGroup.add(point3);
-    this.pointGroup.add(point4);
+    this.createBoardFromGameData();
   },
   update: function() {
   },
   render: function() {
     // this.game.debug.inputInfo(32, 32);
     // this.game.debug.pointer( this.game.input.activePointer );
+  },
+  createBoardFromGameData: function() {
+    this.pointGroup = this.game.add.group();
+    var data = this.game.data;
+    var startPoint = this.game.add.sprite(data.startPoint.x, data.startPoint.y, 'start-end', 0);
+    startPoint.anchor.setTo(0.5, 0.5);
+    startPoint.scale.setTo(0.5, 0.5);
+    this.pointGroup.add(startPoint);
+
+    var endPoint = this.game.add.sprite(data.endPoint.x, data.endPoint.y 'start-end', 1);
+    endPoint.anchor.setTo(0.5, 0.5);
+    endPoint.scale.setTo(0.5, 0.5);
+    this.pointGroup.add(endPoint);
+
+    this.avatar = new Avatar(this.game);
+    this.avatar.moveTo(startPoint);
+    this.game.add.existing(this.avatar);
+
+    for( var point in data.points) {
+      this.pointGroup(new Point(this.game, point.x, point.y));
+    }
   }
 };
 
