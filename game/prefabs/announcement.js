333743796834aa2b81901a1ae51c0b7cb0625f14
Juho Leinonen <juholei@gmail.com>
2015-03-10 12:00:36 +0200

    Experiment with implementing dimming of the screen behind announcement box.

--- game/prefabs/announcement.js
+++ game/prefabs/announcement.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var Announcement = function(game, callback, callbackContext, announcementText) {
-  Phaser.Sprite.call(this, game, game.world.centerX, game.world.centerY, 'answer-background');
+
+  var bitmapData = game.add.bitmapData(1024, 768);
+  bitmapData.fill(0, 0, 0, 0.7);
+  Phaser.Sprite.call(this, game, game.world.centerX, game.world.centerY, bitmapData);
   this.anchor.setTo(0.5, 0.5);
 
-  var button = game.add.button(0, this.height / 4, 'button', callback, callbackContext, 1, 0);
+  var sprite = game.add.sprite(0, 0, 'answer-background');
+  sprite.anchor.setTo(0.5, 0.5);
+  this.addChild(sprite);
+  var button = game.add.button(0, sprite.height / 4, 'button', callback, callbackContext, 1, 0);
   button.anchor.setTo(0.5, 0.5);
 
   var textStyle = {font: '32px Arial', fill: 'white', align: 'center', wordWrap: true, wordWrapWidth: 574};
