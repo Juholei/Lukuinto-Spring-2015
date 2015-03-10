5c7a897af106fc0ab7661b0e7c602736805896bc
Juho Leinonen <juholei@gmail.com>
2015-03-10 13:46:22 +0200

    Testing scaling 1x1 bitmap to fill the screen.

--- game/prefabs/announcement.js
+++ game/prefabs/announcement.js
@@ -1,28 +1,33 @@
 'use strict';
 
 var Announcement = function(game, callback, callbackContext, announcementText) {
+  var t0 = performance.now();
 
-  var bitmapData = game.add.bitmapData(1024, 768);
+  var bitmapData = game.add.bitmapData(1, 1);
   bitmapData.fill(0, 0, 0, 0.7);
   Phaser.Sprite.call(this, game, game.world.centerX, game.world.centerY, bitmapData);
   this.anchor.setTo(0.5, 0.5);
+  this.height = 768;
+  this.width = 1024;
 
-  var sprite = game.add.sprite(0, 0, 'answer-background');
-  sprite.anchor.setTo(0.5, 0.5);
-  this.addChild(sprite);
-  var button = game.add.button(0, sprite.height / 4, 'button', callback, callbackContext, 1, 0);
-  button.anchor.setTo(0.5, 0.5);
+  var t1 = performance.now();
+  console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
+  // var sprite = game.add.sprite(0, 0, 'answer-background');
+  // sprite.anchor.setTo(0.5, 0.5);
+  // this.addChild(sprite);
+  // var button = game.add.button(0, sprite.height / 4, 'button', callback, callbackContext, 1, 0);
+  // button.anchor.setTo(0.5, 0.5);
 
-  var textStyle = {font: '32px Arial', fill: 'white', align: 'center', wordWrap: true, wordWrapWidth: 574};
-  var text = game.add.text(-100, 0, announcementText, textStyle);
-  text.anchor.setTo(0.5, 0.5);
+  // var textStyle = {font: '32px Arial', fill: 'white', align: 'center', wordWrap: true, wordWrapWidth: 574};
+  // var text = game.add.text(-100, 0, announcementText, textStyle);
+  // text.anchor.setTo(0.5, 0.5);
 
-  var buttonText = game.add.text(0, 0, 'OK', textStyle);
-  buttonText.anchor.setTo(0.5, 0.5);
+  // var buttonText = game.add.text(0, 0, 'OK', textStyle);
+  // buttonText.anchor.setTo(0.5, 0.5);
 
-  button.addChild(buttonText);
-  this.addChild(button);
-  this.addChild(text);
+  // button.addChild(buttonText);
+  // this.addChild(button);
+  // this.addChild(text);
 
 };
 
