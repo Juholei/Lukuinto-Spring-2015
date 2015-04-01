e7daa9526f9c0235cf8c33eba57f4cc80aff8639
Juho Leinonen <juholei@gmail.com>
2015-04-01 16:49:36 +0300

    Add body-parser as requirement. It is used to parse json from requests. Implement ajax json post request thing in client. At the moment just in index.html for quick and dirty and easy testing.

--- server/routes/savegamejson.js
+++ server/routes/savegamejson.js
@@ -2,22 +2,24 @@
 var fs = require('fs');
 var pg = require('pg');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 module.exports = function(app) {
-  app.get('/savegamejson', function(req, res, next) {
-    fs.readFile(path.normalize(__dirname + '/../../client/assets/gamedata.json'), function(err, data) {
-      console.log(data.toString());
-      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        if (err) {
-          console.log(err);
-        }
-        client.query('insert into games(data) values($1)', [data.toString()], function(err, writeResult) {
-          console.log(err, writeResult);
-          done();
-          client.end();
-          res.json({status: 'something must have happened'});
-        });
-      });
-    });
+  app.use(bodyParser.json());
+  app.post('/savegamejson', function(req, res, next) {
+    // fs.readFile(path.normalize(__dirname + '/../../client/assets/gamedata.json'), function(err, data) {
+      console.log(req.body);
+      // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      //   if (err) {
+      //     console.log(err);
+      //   }
+      //   client.query('insert into games(data) values($1)', [data.toString()], function(err, writeResult) {
+      //     console.log(err, writeResult);
+      //     done();
+      //     client.end();
+          res.json(req.body);
+        // });
+      // });
+    // });
   });
 };
