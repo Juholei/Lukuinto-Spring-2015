58a8e3b848ab868b20d8cf96fcdbf0b582b169ad
Juho Leinonen <juholei@gmail.com>
2015-04-04 16:15:13 +0300

    Add routes for requesting all the available game IDs and a game with specific ID.

---
+++ server/routes/games.js
@@ -0,0 +1,27 @@
+'use strict';
+var pg = require('pg');
+
+module.exports = function(app) {
+  app.get('/games/', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      if (err) {
+        return console.error('error fetching client from pool', err);
+      }
+
+      client.query('select id from games',
+        function(err, readResult) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if (err) {
+          return console.error('error running query', err);
+        }
+
+        client.end();
+        var json = JSON.stringify(readResult.rows);
+        res.writeHead(200, {'content-type':'application/json', 'content-length':Buffer.byteLength(json)});
+        res.end(json);
+      });
+    });
+  });
+};
